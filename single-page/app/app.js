25cb2160527705e075cb11068914b99649b2b04f
Jorge Oliveira <jso@aleph.inesc-id.pt>
2016-01-12 17:47:19 +0000

    Removed apiController from index and app

--- single-page/app/app.js
+++ single-page/app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("vre", ["ngRoute", "navbar", "footer", "home", "signin", "workflows", "databases", "detail", "api"]);
+var app = angular.module("vre", ["ngRoute", "navbar", "footer", "home", "signin", "workflows", "databases", "detail"]);
 
 app.config(["$routeProvider", function ($routeProvider) {
 	$routeProvider
