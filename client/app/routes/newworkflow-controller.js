77a6b8e2ba31f2fb83ff7e610c0ea396d6bc30db
oliveirajorge88 <pQ,i97&j:1>
2016-01-23 13:15:07 +0000

    added live view of form

--- client/app/routes/newworkflow-controller.js
+++ client/app/routes/newworkflow-controller.js
@@ -7,15 +7,13 @@ angular.module("newworkflow").config(["$routeProvider", function ($routeProvider
 
 angular.module("newworkflow").controller("NewworkflowController", ["$http", function($http){
 
-	this.nflow = {};
-
 	this.addFlow = function(){
-		$http.post('//aleph.inesc-id.pt\:5000/workflows', nflow)
-			.success(function(nflow) {
-				console.log('Successfuly posted new workflow: ' + nflow);
+		$http.post('//aleph.inesc-id.pt\:5000/workflows', this.nflow)
+			.success(function(this.nflow) {
+				console.log('Successfuly posted new workflow: ' + this.nflow);
 			})
 			.error(function(){
-				console.log("Could not insert: " + nflow);
+				console.log("Could not insert: " + this.nflow);
 			});
 	};
 
