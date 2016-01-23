7971a58288f7f6e95f9e60b36243b7f4b6d5322d
oliveirajorge88 <pQ,i97&j:1>
2016-01-23 14:06:43 +0000

    nflow as function parameter

--- client/app/routes/newworkflow-controller.js
+++ client/app/routes/newworkflow-controller.js
@@ -5,20 +5,18 @@ angular.module("newworkflow").config(["$routeProvider", function ($routeProvider
 		.when("/newworkflow", {templateUrl: "app/routes/newworkflow-view.html", controller: "NewworkflowController"});
 }]);
 
-angular.module("newworkflow").controller("NewworkflowController", ["$http", function($http, $scope){
-
-    $scope.nflow = {};
+angular.module("newworkflow").controller("NewworkflowController", ["$http", "$window", "$scope", function($http, $window, $scope){
 
 	$scope.addFlow = function(nflow){
 		$http.post('//aleph.inesc-id.pt\:5000/workflows', nflow)
 			.success(function() {
 				console.log('Successfuly posted new workflow: ' + nflow);
+                $window.location.href = '#/workflows';
 			})
 			.error(function(){
 				console.log("Could not insert: " + nflow);
 			});
 
-        $scope.nflow = {};
 	};
 
 }]);
