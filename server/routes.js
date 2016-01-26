1e7fa86b0bb0c21433d569d5f0fc38cc176fb331
Jorge Oliveira <jso@aleph.inesc-id.pt>
2016-01-26 17:21:03 +0000

    changed location and port of api

--- server/routes.js
+++ server/routes.js
@@ -4,15 +4,15 @@ module.exports = function (app) {
 
 	var handlers = require("require-dir")("./handlers");
 
-	app.get('/databases', handlers["api-databases-get-public"]);
-	app.post('/databases', handlers["api-databases-post-public"]);
+	app.get('/vre/api/databases', handlers["api-databases-get-public"]);
+	app.post('/vre/api/databases', handlers["api-databases-post-public"]);
 	/*	app.get('/databases/:id', handlers["api-databases-get-public-id"]);
 	app.put('/databases/:id', handlers["api-databases-put"]);
 	app.delete('/databases/:id', handlers["api-databases-delete"]);*/
 
-	app.get('/workflows', handlers["api-workflows-get-public"]);
-	app.post('/workflows', handlers["api-workflows-post-public"]);
-	app.get('/workflows/:id', handlers["api-workflows-get-public-id"]);
+	app.get('/vre/api/workflows', handlers["api-workflows-get-public"]);
+	app.post('/vre/api/workflows', handlers["api-workflows-post-public"]);
+	app.get('/vre/api/workflows/:id', handlers["api-workflows-get-public-id"]);
 	/*app.put('/workflows/:id', handlers["api-workflows-put"]);
 	app.delete('/workflows/:id', handlers["api-workflows-delete"]);
 
