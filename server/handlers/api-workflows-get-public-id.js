60d5aea86d99b4de285edc5035456bff12a2600c
oliveirajorge88 <pQ,i97&j:1>
2016-01-26 01:17:14 +0000

    trying to ativate get workflow by id

--- server/handlers/api-workflows-get-public-id.js
+++ server/handlers/api-workflows-get-public-id.js
@@ -10,7 +10,7 @@ handlers.push(function(req, res, next) {
 
     console.log('\n\nTrying to get workflow with id ' + id );
 
-    Promise.try(Workflow.findById(id))
+    Promise(Workflow.findById(id))
         .then(function(workflows){
             workflows = workflows.map(function (workflow) {
                 workflow = workflow.toObject();
