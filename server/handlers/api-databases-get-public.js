c3d2f7e166d7cd090ba0d7b6668969d1628220ca
oliveirajorge88 <pQ,i97&j:1>
2016-01-26 12:50:15 +0000

    trying to do get by id

---
+++ server/handlers/api-databases-get-public.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var handlers = module.exports = [];
+
+handlers.push(function(req, res, next) {
+    var Database = req.app.db.models.Database;
+
+    Promise.all(Database.find())
+        .then(function(databases){
+            databases = databases.map(function (database) {
+                database = database.toObject();
+                return database;
+            });
+
+            res.send({databases});
+        }).catch(next);
+});
