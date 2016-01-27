408df1668c4dd084658e896aadc6552ac29a0776
oliveirajorge88 <pQ,i97&j:1>
2016-01-27 18:31:04 +0000

    added delete workflow by id

---
+++ server/handlers/api-workflows-delete.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var handlers = module.exports = [];
+
+handlers.push(function(req, res, next) {
+
+    var Workflow = req.app.db.models.Workflow;
+
+    var id = req.params.id;
+
+    console.log('\n\nTrying to delete workflow with id ' + id );
+
+    Workflow.remove(id)
+        .then(function(workflow) {
+            res.send({'success'});
+        })
+        .catch(next);
+});
