11301b97feca81e82f23bc5717c4505ed47d8dd2
oliveirajorge88 <pQ,i97&j:1>
2016-01-27 12:02:11 +0000

    strated copying workflow stuff to database. updated some broken api links

--- client/app/components/adddatabase-directive.js
+++ client/app/components/adddatabase-directive.js
@@ -1,6 +1,6 @@
 angular.module("adddatabase", []);
 
-angular.module("adddatabase).directive("adddatabase", function(){
+angular.module("adddatabase").directive("adddatabase", function(){
     return {
         restrict: "E",
         templateUrl: "app/components/adddatabase-view.html"
