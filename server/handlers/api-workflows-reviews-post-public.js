6bf51bccd3936212299038cbcdfbfb5374c42dd2
voxivoid <voxivoid@hotmail.com>
2016-02-01 17:44:53 +0000

    Add review done.

--- server/handlers/api-workflows-reviews-post-public.js
+++ server/handlers/api-workflows-reviews-post-public.js
@@ -17,27 +17,19 @@ handlers.push(function(req, res, next) {
 
     var Review = req.app.db.models.Review;
 
+    var id = req.params.id;
+
     var review = null;
 
     Promise.resolve()
         .then(function () {
             review = new Review(req.body);
 
-            var id = req.params.id;
-
             console.log('\n\nTrying to insert review in workflow with id ' + id + ": " + review);
 
             //return review.save();
         })
         .then(function () {
-            res.send({
-                success: {
-                    message: "Review created.",
-                    workflow: review._id
-                }
-            });
-        })
-        .then(function (req, res, next) {
 
             var Workflow = req.app.db.models.Workflow;
 
@@ -46,7 +38,7 @@ handlers.push(function(req, res, next) {
                     if (!workflow) {
                         res.send({error: id + " workflow doesn't exist"});
                     } else {
-                        Workflow.update(id, {$push: {"reviews": review}}, {safe: true, upsert: true})
+                        Workflow.update(workflow, {$push: {"reviews": review}}, {safe: true, upsert: true})
                             .then(function () {
                                 res.send({success: id + "review added."});
                             });
