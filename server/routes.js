0a9a138e4dc74c920818e8a16f569d30a377e125
voxivoid <voxivoid@hotmail.com>
2016-03-21 12:56:48 +0000

    Is authenticated route

--- server/routes.js
+++ server/routes.js
@@ -5,7 +5,11 @@ module.exports = function (app) {
 
 	app.get('/vre/api/auth/google/callback', handlers["auth-callback"]);
 	app.get('/vre/api/auth/google', app.passport.authenticate('google', { scope : ['profile', 'email'] }));
-
+	app.get('/vre/api/auth/google/isauthenticated', function isAuthenticated(req, res, next) {
+		if(req.isAuthenticated()) res.send({success: "User is logged in."});
+		else res.send({error: "User isn't logged in."});
+	});
+	
 	app.get('/vre/api/:collection',                         handlers["api-get"]);
 	app.get('/vre/api/:collection/:id',                     handlers["api-get-id"]);
     app.delete('/vre/api/delete/:collection/:id',           handlers["api-delete"]);
@@ -19,7 +23,7 @@ module.exports = function (app) {
 	app.put('/vre/api/reviews/:collection/:id',             handlers["api-reviews-put"]);
 	app.put('/vre/api/reviews/:collection/:docid/:revid',   handlers["api-reviews-delete"]);
 
-	function isLoggedIn(req, res, next) {
+	function isAuthenticated(req, res, next) {
 		if (req.isAuthenticated())
 			return next();
 		res.redirect('/');
