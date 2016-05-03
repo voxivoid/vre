8c1aacd3e68dbd646d4afe18ac56c088a9369a6b
oliveirajorge88 <oliveira.jorge.88@gmail.com>
2016-05-03 18:41:05 +0100

    Added google analytics

--- client/app/app.js
+++ client/app/app.js
@@ -1,7 +1,8 @@
 var app = angular.module("vre", ["ngRoute", "navbar", "footer", "home", "signin", "workflows", "protocols", "databases", "pubdatas",
 	"news", "rest", "tools", "start", "about", "tutorials", "profile"]);
 
-angular.module("vre").controller("vreController", ["$scope" ,"$http", function ($scope,$http) {
+angular.module("vre").controller("vreController", ["$scope" ,"$http", "$location", "$window",
+    function ($scope,$http,$location,$window) {
 	$scope.isAuthenticated = false; // used to know if the user is logged in
 
 	$http.get('http://aleph.inesc-id.pt/vre/api/auth/google/isauthenticated')
@@ -16,6 +17,13 @@ angular.module("vre").controller("vreController", ["$scope" ,"$http", function (
 		.error(function () {
 			$scope.isAuthenticated = false;
 		});
+
+
+		$scope.$on('$viewContentLoaded', function(event) {
+			$window.ga('send', 'pageview', { page: $location.url() });
+		});
+
+
 }]);
 
 app.config(["$routeProvider", function ($routeProvider) {
